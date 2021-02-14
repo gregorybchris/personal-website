4bddb2a54cda4c8c1e01e401f5a35422c5a846f7
gregorybchris <gregorybchris@gmail.com>
2021-02-14 01:39:38 -0800

    Fix settings tests

--- backend/src/chris_package/tests/app/test_app.py
+++ backend/src/chris_package/tests/app/test_app.py
@@ -7,14 +7,15 @@ from chris.app.http_codes import HTTPCodes
 class TestApp:
 
     @pytest.fixture
-    def client(self):
+    def client(self, monkeypatch):
+        monkeypatch.setenv('CGME_DATABASE_CONN', 'mock_conn_str')
         app = App()
         app._app.config['TESTING'] = True
 
         with app._app.test_client() as client:
             yield client
 
-    def test_get_projects(self, client):
+    def test_get_projects(self, client, ):
         route = 'v1/projects'
         response = client.get(route)
         assert response.status_code == HTTPCodes.SUCCESS_GENERAL
--- backend/src/chris_package/tests/app/test_settings.py
+++ backend/src/chris_package/tests/app/test_settings.py
@@ -17,10 +17,31 @@ def get_settings():
 
 class TestSettings:
 
-    @pytest.mark.parametrize('setting', get_settings())
-    def test_settings(self, setting):
-        setting_name, env_variable, default_value = setting
-        if os.environ.get(env_variable) is None:
-            assert getattr(settings, setting_name) == default_value
-        else:
-            assert getattr(settings, setting_name) == os.environ[env_variable]
+    def test_basic(self, monkeypatch):
+        setting = settings.EnvSetting('TEST_VARIABLE')
+        monkeypatch.setenv('TEST_VARIABLE', 'value')
+        assert setting.value == 'value'
+
+    def test_default(self, monkeypatch):
+        setting = settings.EnvSetting('TEST_VARIABLE', default='value')
+        monkeypatch.delenv('TEST_VARIABLE', raising=False)
+        assert setting.value == 'value'
+
+    def test_required(self, monkeypatch):
+        setting = settings.EnvSetting('TEST_VARIABLE', required=True)
+        monkeypatch.delenv('TEST_VARIABLE', raising=False)
+        with pytest.raises(OSError, match="Environment variable TEST_VARIABLE is not set"):
+            setting.value
+
+    @pytest.mark.parametrize('setting_name_pair', [
+        (settings.FLASK_RUN_PORT, 'FLASK_RUN_PORT'),
+        (settings.FLASK_DEBUG, 'FLASK_DEBUG'),
+        (settings.FLASK_HOST, 'FLASK_HOST'),
+        (settings.LOG_FILE_NAME, 'CGME_LOG_FILE'),
+        (settings.INSTRUMENTATION_KEY, 'CGME_TELEMETRY_KEY'),
+        (settings.DATABASE_CONN_STRING, 'CGME_DATABASE_CONN'),
+        (settings.DATABASE_NAME, 'CGME_DATABASE_NAME'),
+    ])
+    def test_vars(self, setting_name_pair):
+        setting, setting_name = setting_name_pair
+        assert setting.var == setting_name
