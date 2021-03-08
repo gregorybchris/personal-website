6301d6a4a4c39f82c0ce4990f20d13cffb590d36
gregorybchris <gregorybchris@gmail.com>
2021-03-07 18:54:44 -0800

    Update docstrings for database and settings

--- backend/src/chris_package/chris/app/database_utilities.py
+++ backend/src/chris_package/chris/app/database_utilities.py
@@ -1,14 +1,20 @@
+"""Utilities for MongoDB operations."""
 from pymongo import MongoClient
 
 from chris.app import settings
 
 
 class CollectionNames:
+    """Names of MongoDB collections."""
+
     SURVEYS = 'surveys'
 
 
 class DatabaseCollection:
+    """Wrapper for MongoDB collection operations."""
+
     def __init__(self, collection_name: str):
+        """Construct a DatabaseCollection."""
         self._collection_name = collection_name
 
         connection_string = settings.DATABASE_CONN_STRING.value
@@ -17,10 +23,13 @@ class DatabaseCollection:
         self._dbc = self._db[collection_name]
 
     def find(self, document={}):
+        """Find documents in the MongoDB collection."""
         return self._dbc.find(document)
 
     def find_one(self, document):
+        """Find one document in the MongoDB collection."""
         return self._dbc.find_one(document)
 
     def insert_one(self, document):
+        """Insert one document into the MongoDB collection."""
         return self._dbc.insert_one(document)
