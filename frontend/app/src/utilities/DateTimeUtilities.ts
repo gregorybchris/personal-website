1bd8ad68a9bdd806b6d99dc8ca4401582a29d691
gregorybchris <gregorybchris@gmail.com>
2021-04-26 15:11:15 -0700

    Fix date format bug

--- frontend/app/src/utilities/DateTimeUtilities.ts
+++ frontend/app/src/utilities/DateTimeUtilities.ts
@@ -2,7 +2,7 @@ const formatDate = (dateString: string) => {
   const date = new Date(dateString);
   const yearNumber = date.getFullYear();
   const monthNumber = date.getMonth();
-  const dayNumber = date.getDate();
+  const dayNumber = date.getDate() + 1;
   const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const monthName = monthNames[monthNumber];
   return `${monthName}. ${dayNumber}, ${yearNumber}`;
