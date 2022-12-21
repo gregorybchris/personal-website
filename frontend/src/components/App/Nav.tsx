c253c13f829b21ae3bbf65af100abc809560aa97
Chris Gregory <gregorybchris@gmail.com>
2022-12-21 00:21:28 -0800

    Add running and music to top tabs

--- frontend/src/components/App/Nav.tsx
+++ frontend/src/components/App/Nav.tsx
@@ -17,15 +17,18 @@ function Nav() {
       <NavLink to="/" className={getLinkClassName("Nav-page-link")}>
         <span className="Nav-page-link-text">Home</span>
       </NavLink>
-
       <NavLink to="/code" className={getLinkClassName("Nav-page-link")}>
         <span className="Nav-page-link-text">Code</span>
       </NavLink>
-
+      <NavLink to="/running" className={getLinkClassName("Nav-page-link")}>
+        <span className="Nav-page-link-text">Running</span>
+      </NavLink>
+      <NavLink to="/music" className={getLinkClassName("Nav-page-link")}>
+        <span className="Nav-page-link-text">Music</span>
+      </NavLink>
       <NavLink to="/links" className={getLinkClassName("Nav-page-link")}>
         <span className="Nav-page-link-text">Links</span>
       </NavLink>
-
       <NavLink to="/contact" className={getLinkClassName("Nav-page-link")}>
         <span className="Nav-page-link-text">Contact</span>
       </NavLink>
