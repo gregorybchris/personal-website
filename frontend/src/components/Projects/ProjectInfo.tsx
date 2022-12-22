32e3e9eaf09bb5563c4942a93cd115324331f42e
Chris Gregory <gregorybchris@gmail.com>
2022-12-21 17:54:17 -0800

    Fix projects download

--- frontend/src/components/Projects/ProjectInfo.tsx
+++ frontend/src/components/Projects/ProjectInfo.tsx
@@ -6,7 +6,7 @@ import { formatDate } from "../../utilities/datetimeUtilities";
 
 interface ProjectInfoProps {
   currentProject: ProjectModel | null;
-  onProjectDownload: (project: ProjectModel, downloadLink: string) => void;
+  onProjectDownload: (project: ProjectModel) => void;
 }
 
 export default function ProjectInfo(props: ProjectInfoProps) {
@@ -20,16 +20,14 @@ export default function ProjectInfo(props: ProjectInfoProps) {
             <div className="ProjectInfo-item ProjectInfo-name">{project.name}</div>
             <div className="ProjectInfo-item">{formatDate(project.date)}</div>
             <div className="ProjectInfo-item ProjectInfo-description">{project.description}</div>
-            {project.download_link && (
-              <DownloadLink project={project} onDownload={props.onProjectDownload} url={project.download_link} />
-            )}
+            {project.download_link && <DownloadLink project={project} onDownload={props.onProjectDownload} />}
             {project.web_link && <ProjectInfoLink text="Live demo" link={project.web_link} />}
             {project.source_link && <ProjectInfoLink text="Source code" link={project.source_link} />}
           </div>
           <div className="ProjectInfo-images">
             {project.image_links.length === 0 && <div className="ProjectInfo-item">[no images available]</div>}
             {project.image_links.length !== 0 &&
-              project.image_links.map((imageLink) => <ProjectImage url={imageLink} />)}
+              project.image_links.map((imageLink, i) => <ProjectImage key={i} url={imageLink} />)}
           </div>
         </div>
       )}
@@ -51,15 +49,11 @@ function ProjectInfoLink(props: { text: string; link: string }) {
   );
 }
 
-function DownloadLink(props: {
-  url: string;
-  project: ProjectModel;
-  onDownload: (project: ProjectModel, downloadLink: string) => void;
-}) {
+function DownloadLink(props: { project: ProjectModel; onDownload: (project: ProjectModel) => void }) {
   const isJava = props.project.primary_language === "Java";
   return (
     <div className="ProjectInfo-item">
-      <span className="Common-simple-link" onClick={() => props.onDownload(props.project, props.url)}>
+      <span className="Common-simple-link" onClick={() => props.onDownload(props.project)}>
         {isJava ? "Download Java runnable archive" : "Download project"}
       </span>
       {isJava && <InfoIcon text="Requires a Java installation to run." />}
