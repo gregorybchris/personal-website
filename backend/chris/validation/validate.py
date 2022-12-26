026a2b2b8626406eca9ba0cb1d13e42629518e84
Chris Gregory <gregorybchris@gmail.com>
2022-12-25 23:15:18 -0800

    Simplify validate CLI output

--- backend/chris/validation/validate.py
+++ backend/chris/validation/validate.py
@@ -1,5 +1,6 @@
 """Post validator."""
 from jsonvl import Validator, validate_file
+from jsonvl.errors import JsonValidationError
 from pathlib import Path
 from chris.validation.constraints import MonotoneIncreaseConstraint
 
@@ -9,10 +10,14 @@ SCHEMAS_DIRPATH = Path(__file__).parent / "schemas"
 
 def validate_posts() -> None:
     """Validate all posts."""
-    validate_file(
-        DATA_DIRPATH / "blog" / "posts.json",
-        SCHEMAS_DIRPATH / "posts-schema.json",
-    )
+    try:
+        validate_file(
+            DATA_DIRPATH / "blog" / "posts.json",
+            SCHEMAS_DIRPATH / "posts-schema.json",
+        )
+    except JsonValidationError as e:
+        print("Validation failed: posts")
+        print(e)
     print("Validation succeeded: posts")
 
 
@@ -20,10 +25,14 @@ def validate_projects() -> None:
     """Validate all projects."""
     validator = Validator()
     validator.register_constraint(MonotoneIncreaseConstraint(), "array", "monotone_inc")
-    validator.validate_file(
-        DATA_DIRPATH / "projects" / "projects.json",
-        SCHEMAS_DIRPATH / "projects-schema.json",
-    )
+    try:
+        validator.validate_file(
+            DATA_DIRPATH / "projects" / "projects.json",
+            SCHEMAS_DIRPATH / "projects-schema.json",
+        )
+    except JsonValidationError as e:
+        print("Validation failed: projects")
+        print(e)
     print("Validation succeeded: projects")
 
 
@@ -31,26 +40,38 @@ def validate_hikes() -> None:
     """Validate all hikes."""
     validator = Validator()
     validator.register_constraint(MonotoneIncreaseConstraint(), "array", "monotone_inc")
-    validator.validate_file(
-        DATA_DIRPATH / "outdoor" / "hiking-routes.json",
-        SCHEMAS_DIRPATH / "hiking-routes-schema.json",
-    )
+    try:
+        validator.validate_file(
+            DATA_DIRPATH / "outdoor" / "hiking-routes.json",
+            SCHEMAS_DIRPATH / "hiking-routes-schema.json",
+        )
+    except JsonValidationError as e:
+        print("Validation failed: hikes")
+        print(e)
     print("Validation succeeded: hikes")
 
 
 def validate_jobs() -> None:
     """Validate all jobs."""
-    validate_file(
-        DATA_DIRPATH / "professional" / "jobs.json",
-        SCHEMAS_DIRPATH / "jobs-schema.json",
-    )
+    try:
+        validate_file(
+            DATA_DIRPATH / "professional" / "jobs.json",
+            SCHEMAS_DIRPATH / "jobs-schema.json",
+        )
+    except JsonValidationError as e:
+        print("Validation failed: jobs")
+        print(e)
     print("Validation succeeded: jobs")
 
 
 def validate_books() -> None:
     """Validate all books."""
-    validate_file(
-        DATA_DIRPATH / "media" / "books.json",
-        SCHEMAS_DIRPATH / "books-schema.json",
-    )
+    try:
+        validate_file(
+            DATA_DIRPATH / "media" / "books.json",
+            SCHEMAS_DIRPATH / "books-schema.json",
+        )
+    except JsonValidationError as e:
+        print("Validation failed: books")
+        print(e)
     print("Validation succeeded: books")
