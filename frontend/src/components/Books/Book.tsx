ad42de9fb411d53d8b33e3df5d92ef2cc7c4768c
Chris Gregory <gregorybchris@gmail.com>
2024-01-03 01:50:44 -0800

    Invalidate image cache

--- frontend/src/components/Books/Book.tsx
+++ frontend/src/components/Books/Book.tsx
@@ -15,7 +15,7 @@ export default function Book(props: BookProps) {
     <div className="mb-4">
       <a href={props.book.goodreads_link} target="_blank">
         <div className="py-4 flex justify-center">
-          <img src={`${imageLink}?a=2`} className="w-48" />
+          <img src={`${imageLink}?a=3`} className="w-48" />
         </div>
 
         <div className="font-bold text-center">{props.book.title}</div>
