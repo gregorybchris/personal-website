4be57266e33a45ed21f5f9d3f8163a7d340170f0
Chris Gregory <gregorybchris@gmail.com>
2024-01-06 20:09:56 -0800

    Music page style

--- frontend/src/pages/MusicPage.tsx
+++ frontend/src/pages/MusicPage.tsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export function MusicPage() {
   return (
     <div className="p-8">
-      <div className="mx-auto w-[90%] pb-5 text-center md:w-[80%]">
+      <div className="mx-auto w-[90%] pb-2 text-center md:w-[80%]">
         <div className="block pb-3 font-noto text-3xl font-bold text-text-1">
           Music
         </div>
@@ -18,18 +18,15 @@ export function MusicPage() {
           </Link>{" "}
           page!
         </div>
-        <div className="mx-auto block w-[95%] py-3 font-raleway text-text-1 md:w-[70%]">
-          Click on the album cover below to listen to full songs.
-        </div>
       </div>
 
-      <div className="mx-auto mt-5 w-[95%] max-w-[500px]">
+      <div className="mx-auto mt-3 w-[95%] max-w-[500px]">
         <iframe
           title="latest-music"
           className="w-full"
           src="https://open.spotify.com/embed/playlist/0oLhscbRKBJdfA5gfNQbe3"
           width="500"
-          height="380"
+          height="420"
           allow="encrypted-media"
         ></iframe>
       </div>
