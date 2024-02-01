46d37f1ae77a776b81914e1a372c313730e07d08
Chris Gregory <gregorybchris@gmail.com>
2024-01-31 17:39:21 -0800

    Add text to watermark

--- frontend/src/pages/WatermarkPage.tsx
+++ frontend/src/pages/WatermarkPage.tsx
@@ -3,8 +3,13 @@ import logo from "../assets/icons/logo-blue.svg";
 export function WatermarkPage() {
   return (
     <div className="flex h-[600px] items-center justify-center">
-      <div className="opacity-40">
-        <img src={logo} alt="Your SVG" />
+      <div className="relative">
+        <div className="absolute top-1/2 flex w-full items-center text-center font-noto text-4xl font-bold text-text-1">
+          <div className="absolute -top-10 w-full text-center">
+            ChrisGregory.me
+          </div>
+        </div>
+        <img src={logo} alt="Your SVG" className="w-[400px] opacity-20" />
       </div>
     </div>
   );
