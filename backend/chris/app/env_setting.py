1132e9fe8b6c1f0cef4a26c3d819f00bc2cc9b2c
Chris Gregory <gregorybchris@gmail.com>
2024-02-05 00:20:23 -0800

    Simplify EnvSetting

--- backend/chris/app/env_setting.py
+++ backend/chris/app/env_setting.py
@@ -1,25 +1,17 @@
 import os
+from dataclasses import dataclass
 from typing import Optional
 
 
+@dataclass
 class EnvSetting:
-
-    def __init__(self, variable: str, default: Optional[str] = None, required: bool = False):
-        self._variable = variable
-        self._default = default
-        self._required = required
-
-    @property
-    def var(self) -> str:
-        return self._variable
-
-    @property
-    def default(self) -> Optional[str]:
-        return self._default
+    var: str
+    default: Optional[str] = None
+    required: bool = False
 
     @property
     def value(self) -> Optional[str]:
-        variable_value = os.getenv(self._variable, self._default)
-        if variable_value is None and self._required:
+        variable_value = os.getenv(str(self.var), self.default)
+        if variable_value is None and self.required:
             raise EnvironmentError(f"Environment variable {self.var} is not set")
         return variable_value
