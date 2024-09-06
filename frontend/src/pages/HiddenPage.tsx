1bf8bd8be1d17c83b3ec3d03a40f32f527187732
Chris Gregory <gregorybchris@gmail.com>
2024-09-05 17:34:29 -0700

    Update hidden page

--- frontend/src/pages/HiddenPage.tsx
+++ frontend/src/pages/HiddenPage.tsx
@@ -14,86 +14,132 @@ export function HiddenPage() {
       </div>
       <div className="mx-auto w-[70%] py-4">
         <div>
-          <Link to="/hidden/archive" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Archive
+          <div className="py-2">
+            <div className="inline-block pt-2 font-noto text-lg text-text-3 transition-all">
+              Media
             </div>
-          </Link>
-          <Link
-            to="/hidden/blog-graph"
-            className="block font-raleway text-text-1"
-          >
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Blog Graph
+            <Link to="/hidden/shows" className="block font-raleway text-text-1">
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                TV Shows
+              </div>
+            </Link>
+            <Link
+              to="/hidden/channels"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                YouTube Channels
+              </div>
+            </Link>
+            <Link
+              to="/hidden/movies"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Movies
+              </div>
+            </Link>
+            <Link
+              to="/hidden/podcasts"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Podcasts
+              </div>
+            </Link>
+            <Link
+              to="/hidden/tiktoks"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                TikToks
+              </div>
+            </Link>
+            <Link to="/hidden/memes" className="block font-raleway text-text-1">
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Memes
+              </div>
+            </Link>
+          </div>
+
+          <div className="py-2">
+            <div className="inline-block pt-2 font-noto text-lg text-text-3 transition-all">
+              Link Blog
             </div>
-          </Link>
-          <Link to="/hidden/places" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Places
+            <Link to="/links" className="block font-raleway text-text-1">
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Blog
+              </div>
+            </Link>
+            <Link
+              to="/hidden/blog-graph"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Blog Graph
+              </div>
+            </Link>
+          </div>
+
+          <div className="py-2">
+            <div className="inline-block pt-2 font-noto text-lg text-text-3 transition-all">
+              In progress
             </div>
-          </Link>
-          <Link to="/hidden/surveys" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Surveys
+            <Link
+              to="/hidden/places"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Places
+              </div>
+            </Link>
+            <Link
+              to="/hidden/recipes"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Recipes
+              </div>
+            </Link>
+            <Link
+              to="/hidden/watermark"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Site watermark
+              </div>
+            </Link>
+            <Link
+              to="/hidden/projects"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Project logos
+              </div>
+            </Link>
+          </div>
+
+          <div className="py-2">
+            <div className="inline-block pt-2 font-noto text-lg text-text-3 transition-all">
+              Old
             </div>
-          </Link>
-          <Link to="/hidden/shows" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              TV Shows
-            </div>
-          </Link>
-          <Link
-            to="/hidden/channels"
-            className="block font-raleway text-text-1"
-          >
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              YouTube Channels
-            </div>
-          </Link>
-          <Link to="/hidden/movies" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Movies
-            </div>
-          </Link>
-          <Link
-            to="/hidden/podcasts"
-            className="block font-raleway text-text-1"
-          >
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Podcasts
-            </div>
-          </Link>
-          <Link to="/hidden/recipes" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Recipes
-            </div>
-          </Link>
-          <Link
-            to="/hidden/watermark"
-            className="block font-raleway text-text-1"
-          >
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Watermark
-            </div>
-          </Link>
-          <Link
-            to="/hidden/projects"
-            className="block font-raleway text-text-1"
-          >
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Projects
-            </div>
-          </Link>
-          <Link to="/hidden/tiktoks" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              TikToks
-            </div>
-          </Link>
-          <Link to="/hidden/memes" className="block font-raleway text-text-1">
-            <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
-              Memes
-            </div>
-          </Link>
+            <Link
+              to="/hidden/archive"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Archive
+              </div>
+            </Link>
+            <Link
+              to="/hidden/surveys"
+              className="block font-raleway text-text-1"
+            >
+              <div className="inline-block border-b border-background px-2 pt-2 transition-all hover:border-accent">
+                Surveys
+              </div>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
