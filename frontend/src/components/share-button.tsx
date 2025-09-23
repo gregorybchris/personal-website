ecdd4d32d065532bfeecd74833d98ea505860735
Chris Gregory <gregorybchris@gmail.com>
2025-09-22 23:46:00 -0700

    Add back to posts button

--- frontend/src/components/share-button.tsx
+++ frontend/src/components/share-button.tsx
@@ -2,15 +2,21 @@ import { ExportIcon } from "@phosphor-icons/react";
 
 interface ShareButtonProps {
   url: string;
-  title?: string;
-  text?: string;
+  text: string;
+  shareTitle?: string;
+  shareText?: string;
 }
 
-export default function ShareButton({ url, title, text }: ShareButtonProps) {
+export default function ShareButton({
+  url,
+  text,
+  shareTitle,
+  shareText,
+}: ShareButtonProps) {
   const onClick = async () => {
     if (navigator.share) {
       try {
-        await navigator.share({ url, title, text });
+        await navigator.share({ url, title: shareTitle, text: shareText });
       } catch (err) {
         console.error("Share canceled or failed: ", err);
       }
@@ -22,10 +28,10 @@ export default function ShareButton({ url, title, text }: ShareButtonProps) {
   return (
     <div
       onClick={onClick}
-      className="bg-parchment flex cursor-pointer flex-row items-center gap-2 rounded-md px-3 py-1 hover:bg-black/5"
+      className="flex cursor-pointer flex-row items-center gap-2 rounded-md px-3 py-1 hover:bg-black/5"
     >
       <ExportIcon size={20} weight="duotone" color="#6283c0" />
-      <span className="text-md font-medium">Share</span>
+      <span className="text-md">{text}</span>
     </div>
   );
 }
--- frontend/src/pages/blog-post-page.tsx
+++ frontend/src/pages/blog-post-page.tsx
@@ -1,5 +1,6 @@
+import { NotePencilIcon } from "@phosphor-icons/react";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { MarkdownRenderer } from "../components/markdown-renderer";
 import ShareButton from "../components/share-button";
 import "../styles/blog.css";
@@ -11,6 +12,7 @@ import { BlogPost } from "./blog-page";
 export function BlogPostPage() {
   const [currentPost, setCurrentPost] = useState<BlogPost | null>(null);
   const { slug } = useParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     const query = makeQuery(`blog/posts/${slug}`);
@@ -27,7 +29,7 @@ export function BlogPostPage() {
       {currentPost === null ? (
         <span className="">Loading post...</span>
       ) : (
-        <div className="flex w-full flex-col items-center gap-6 px-0">
+        <div className="flex w-full flex-col items-center gap-6 px-0 pb-4">
           <div className="flex flex-col items-center gap-2 md:max-w-[700px]">
             <div className="text-center text-2xl font-bold text-balance md:max-w-[500px] md:text-3xl">
               {currentPost.title}
@@ -37,12 +39,22 @@ export function BlogPostPage() {
             </div>
           </div>
 
-          <div className="w-[100%] pb-6 text-justify leading-relaxed md:w-[50%]">
+          <div className="w-[100%] text-justify leading-relaxed md:w-[50%]">
             <MarkdownRenderer>{currentPost.content}</MarkdownRenderer>
           </div>
 
-          {/* Use pathname instead of href to exclude hash fragment and/or query params */}
-          <ShareButton url={window.location.pathname} />
+          <div className="flex flex-row items-center gap-4 py-2">
+            <div
+              className="flex cursor-pointer flex-row items-center gap-2 rounded-md px-3 py-1 hover:bg-black/5"
+              onClick={() => navigate("/blog")}
+            >
+              <NotePencilIcon size={20} weight="duotone" color="#6283c0" />
+              <span className="text-md">Back to posts</span>
+            </div>
+
+            {/* Use pathname instead of href to exclude hash fragment and/or query params */}
+            <ShareButton text="Share post" url={window.location.pathname} />
+          </div>
         </div>
       )}
     </div>
